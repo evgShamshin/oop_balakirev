b10196e35098d58e1d0ba01e87abb11a30ffb57b
evg_shamshin <i@eshamshin.ru>
2024-10-27 23:37:31 +0300

    2024.10.27

--- 1_introduction/1_4_class methods and self/1.4.9.py
+++ 1_introduction/1_4_class methods and self/1.4.9.py
@@ -1,7 +1,7 @@
 import sys as s
 
 # программу не менять, только добавить два метода
-lst_in = list(map(str.split, s.stdin.readlines()))  # считывание списка строк из входного потока
+lst_in = list(map(str.strip, s.stdin.readlines()))  # считывание списка строк из входного потока
 
 
 class DataBase:
@@ -10,10 +10,18 @@ class DataBase:
 
     # здесь добавлять методы
     def insert(self, data):
-        l = []
-        print([l.append({self.FIELDS[n]: d[n]}) for n, d in enumerate(data)])
+        lst_temp = {}
+        for n_1, d in enumerate(data):
+            for n_2, _ in enumerate(data[n_1]):
+                lst_temp.update({self.FIELDS[n_2]: d[n_2]})
+            self.lst_data.append(lst_temp)
+            lst_temp = {}
 
+    def select(self, a, b):
+        if b > len(self.lst_data):
+            return self.lst_data[a:]
+        else:
+            return self.lst_data[a:b]
 
 db = DataBase()
 db.insert(lst_in)
-print(lst_in)
