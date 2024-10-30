20b9b681b271d7a8d38c39fdc045abe582cb54ef
evg_shamshin <i@eshamshin.ru>
2024-10-31 00:30:03 +0300

    2024.10.31

--- 1_introduction/1_5_init and del/1.5.4.py
+++ 1_introduction/1_5_init and del/1.5.4.py
@@ -1,15 +1,20 @@
-from random import randrange as r, choice as ch
+from random import choice as ch
 
 class Line:
-    def __init__(self, a, b, c, d):
-        self.a, self.b, self.c, self.d = 0, 0, 0, 0
+    def __init__(self, a=0, b=0, c=0, d=0):
+        self.sp, self.ep, = (a, b), (c, d)
 class Rect:
-    def __init__(self, a, b, c, d):
-        self.a, self.b, self.c, self.d = a, b, c, d
+    def __init__(self, a=0, b=0, c=0, d=0):
+        self.sp, self.ep, = (a, b), (c, d)
 
 class Ellipse:
-    def __init__(self, a, b, c, d):
-        self.a, self.b, self.c, self.d = a, b, c, d
+    def __init__(self, a=0, b=0, c=0, d=0):
+        self.sp, self.ep, = (a, b), (c, d)
 
 _class = [Line, Rect, Ellipse]
-elements = [ch(_class)(0,1,3,4) for i in range(217)]
+elements = [ch(_class)(2,1,3,4) for i in range(217)]
+
+for n, i in enumerate(elements):
+    if i.__class__.__name__ == 'Line':
+        i.ep, i.sp = (0, 0), (0, 0)
+
