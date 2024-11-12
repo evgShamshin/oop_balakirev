a14ca3c550dc11702654b73bb317a1066e25a336
evg_shamshin <i@eshamshin.ru>
2024-11-12 23:53:39 +0300

    2024.11.12

--- 1_introduction/1_5_init and del/1.5.9.py
+++ 1_introduction/1_5_init and del/1.5.9.py
@@ -1,1 +1,15 @@
-pass
+from sys import stdin as s
+
+
+class ListObject:
+    def __init__(self, data, next_obj=None):
+        self.data = data
+        self.next_obj = next_obj
+
+    def link(self, obj):
+        self.next_obj = obj[:]
+
+
+lst_in = list(map(str.strip, s.readlines()))
+
+head_obj = ListObject(lst_in[0], lst_in[1])
