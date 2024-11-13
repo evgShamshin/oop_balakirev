89dbf0861f1d43eb1a54d7f27aceed4fa3f2118e
evg_shamshin <i@eshamshin.ru>
2024-11-13 23:42:16 +0300

    2024.11.13

--- 1_introduction/1_5_init and del/1.5.9.py
+++ 1_introduction/1_5_init and del/1.5.9.py
@@ -7,9 +7,13 @@ class ListObject:
         self.next_obj = next_obj
 
     def link(self, obj):
-        self.next_obj = obj[:]
-
+        self.next_obj = obj
+        return self
 
 lst_in = list(map(str.strip, s.readlines()))
 
 head_obj = ListObject(lst_in[0], lst_in[1])
+lst_obj = [ListObject(lst_in[n], lst_in[n + 1]) for n, i in enumerate(lst_in[:-1])]
+lst_obj.append(ListObject(lst_in[-1]))
+lst_obj[0] = head_obj
+res = [lst_obj[n].link(lst_obj[n + 1]).__dict__ for n, i in enumerate(lst_obj[:-1])]
