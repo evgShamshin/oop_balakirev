e8bd747f8b7d840a2a4e6dfd89e7662d036c9cc6
evg_shamshin <i@eshamshin.ru>
2024-11-15 23:47:33 +0300

    2024.11.14

--- 1_introduction/1_5_init and del/1.5.10.py
+++ 1_introduction/1_5_init and del/1.5.10.py
@@ -7,8 +7,31 @@ class Cell:
         :param mine: наличие/отсутствие мины в текущей клетке (True/False)
         :param fl_open: открыта/закрыта клетка - булево значение (True/False). Изначально все клетки закрыты (False)
         """
-        pass
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = fl_open
+
 
 class GamePole:
     """ Управление игровым полем, размером N x N клеток """
-    pass
+
+    def __init__(self, N, M):
+        """
+        :param N: размер поля
+        :param M: общее число мин на поле
+        """
+        self.N = N
+        self.M = M
+
+    def show(self):
+        """
+        Отображение поля в консоли в виде таблицы чисел открытых клеток
+        (# - клетка не открыта;
+        * - мина;
+        между клетками при отображении ставить пробел)
+        """
+        return [["#" * self.N] for _ in range(self.M)]
+
+
+pole_game = GamePole(5, 4)
+print(pole_game.show())
