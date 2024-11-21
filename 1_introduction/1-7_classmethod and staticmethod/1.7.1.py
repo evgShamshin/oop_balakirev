c148ba62822325bedd5aef336695625b5211df8d
evg_shamshin <i@eshamshin.ru>
2024-11-21 23:12:24 +0300

    2024.11.21

---
+++ 1_introduction/1-7_classmethod and staticmethod/1.7.1.py
@@ -0,0 +1,8 @@
+class Stepik:
+    def get_certificate(self):
+        return False
+
+st = Stepik()
+
+st.get_certificate()
+Stepik.get_certificate(st)
---
+++ 1_introduction/1-7_classmethod and staticmethod/1.7.2.py
@@ -0,0 +1,17 @@
+class Loader:
+    @classmethod
+    def json_parse(cls):
+        return ""
+
+
+Loader.json_parse()
+
+# ld.json_parse(Loader)
+
+# Loader.json_parse(ld)
+
+res = Loader.json_parse()
+
+# ld.json_parse()
+
+# res = ld.json_parse()
