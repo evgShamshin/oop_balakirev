c35e63f24dc00aea8105353de07005f127aa6331
evg_shamshin <i@eshamshin.ru>
2024-11-27 23:41:59 +0300

    2024.11.25

--- 1_introduction/1-8_properties and methods/1.8.1.py
+++ 1_introduction/1-8_properties and methods/1.8.1.py
@@ -1,39 +1,39 @@
 class Server:
     buffer = []
-    counter = 0
+    count = 0
+    router_obj = None
 
-    def __new__(cls, *args, **kwargs):
-        cls.counter = + 1
-        return cls.counter
-
-    def __init__(self, *args, **kwargs):
-        self.counter =+ 1
-        self.ip = self.counter
+    def __init__(self):
+        Server.count += 1
+        self.ip = Server.count
 
     def send_data(self, data):
-        pass
+        self.router_obj.buffer.setdefault(self, data)
 
     def get_data(self):
-        pass
+        setattr(self, "zalupa", self.buffer)
+        self.buffer = []
+        print(self.zalupa)
+        return self.zalupa
+
 
     def get_ip(self):
-        pass
+        return self.ip
 
 
 class Router:
     buffer = {}
 
     def link(self, server):
-        setattr(self, "server", server)
+        server.router_obj = self
 
     def unlink(self, server):
         delattr(self, server)
 
-    def send_data(self, data, ip):
-        self.buffer.setdefault(ip, data)
-        print(self.buffer)
-        self.buffer = {}
-        print(self.buffer)
+    def send_data(self):
+        for server in self.buffer:
+            print(server)
+        self.buffer.clear()
 
 
 class Data:
@@ -42,9 +42,58 @@ class Data:
         self.ip = ip
 
 
-# router = Router()
+
+router = Router()
+sv_from = Server()
+sv_from2 = Server()
+router.link(sv_from)
+router.link(sv_from2)
+router.link(Server())
+router.link(Server())
+sv_to = Server()
+router.link(sv_to)
+sv_from.send_data(Data("Hello", sv_to.get_ip()))
+sv_from2.send_data(Data("Hello", sv_to.get_ip()))
+sv_to.send_data(Data("Hi", sv_from.get_ip()))
+router.send_data()
+msg_lst_from = sv_from.get_data()
+msg_lst_to = sv_to.get_data()
+
+
+"""
+ZALUPA EBANAYA BLIAT, IDI NA HUY
+data1 = Data('data1', 1)
+data2 = Data('data2', 2)
+data3 = Data('data3', 3)
+
+router = Router()
+
 sv_from1 = Server()
-# router.send_data("data", "ip")
-print(sv_from1)
-# sv_from2 = Server()
-# print(sv_from2)
+sv_from2 = Server()
+sv_from3 = Server()
+
+print(f"sv_from1 - {sv_from1}")
+print(f"sv_from2 - {sv_from2}")
+print(f"sv_from3 - {sv_from3}")
+print()
+print(f"sv_from1.__dict__ - {sv_from1.__dict__}")
+print(f"sv_from2.__dict__ - {sv_from2.__dict__}")
+print(f"sv_from3.__dict__ - {sv_from3.__dict__}")
+print()
+
+router.link(sv_from1)
+router.link(sv_from2)
+router.link(sv_from3)
+
+print(f"router.list_server - {router.list_server}")
+print(f"sv_from1.router_obj - {sv_from1.router_obj}")
+print(f"sv_from2.router_obj - {sv_from2.router_obj}")
+print(f"sv_from3.router_obj - {sv_from3.router_obj}")
+print()
+
+sv_from1.send_data(data1)
+sv_from2.send_data(data2)
+
+print(f"router.buffer - {router.buffer}")
+
+router.send_data(data1, sv_from1.ip)"""
