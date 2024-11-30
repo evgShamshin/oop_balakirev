800a526cf3a9900728afc2917a2f76ee9fdbd454
evg_shamshin <i@eshamshin.ru>
2024-12-01 00:08:37 +0300

    2024.12.01

--- 2_access modes, properties and descriptors/2-1_public, private, protected/2.1.4.py
+++ 2_access modes, properties and descriptors/2-1_public, private, protected/2.1.4.py
@@ -1,5 +1,26 @@
 class Money:
-    def __init__(self, money):
-        self.__money = money
+    def __init__(self, mn):
+        self.__money = mn
 
-    def set_money(self, money):
+    def set_money(self, mn):
+        if self.check_money(mn): self.__money = mn
+
+    def get_money(self):
+        return self.__money
+
+    def add_money(self, mn):
+        self.__money += mn.get_money()
+
+    def check_money(self, mn):
+        return type(mn) == int and mn > 0
+
+
+mn_1 = Money(10)
+mn_2 = Money(20)
+mn_1.set_money(100)
+mn_2.add_money(mn_1)
+m1 = mn_1.get_money()    # 100
+m2 = mn_2.get_money()    # 120
+
+print(m1)
+print(m2)
