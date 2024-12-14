004c1fa79a98be52ef34396b332319ccb270f0c8
evg_shamshin <i@eshamshin.ru>
2024-12-14 23:43:08 +0300

    2024.12.12

--- 2_access modes, properties and descriptors/2-3_data descriptor and non-data descriptor/2.3.10.py
+++ 2_access modes, properties and descriptors/2-3_data descriptor and non-data descriptor/2.3.10.py
@@ -1,24 +1,44 @@
-class Telecast_Descriptor:
-    def __set_name__(self, owner, name):
-        self.name = '__' + name
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
-
-    def __set__(self, instance, value):
-        setattr(instance, self.name, value)
-
-
 class Telecast:
-    uid = Telecast_Descriptor()
-    name = Telecast_Descriptor()
-    duration = Telecast_Descriptor()
+    def __init__(self, id, name, duration):
+        self.__id = id
+        self.__name = name
+        self.__duration = duration
 
-    def __init__(self, uid, name, duration):
-        self.uid = uid
-        self.name = name
-        self.duration = duration
+    @property
+    def uid(self):
+        return self.__id
 
+    @uid.setter
+    def uid(self, value):
+        self.__id = value
+
+    @uid.deleter
+    def uid(self):
+        self.__id = None
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @name.deleter
+    def name(self):
+        self.__name = None
+
+    @property
+    def duration(self):
+        return self.__duration
+
+    @duration.setter
+    def name(self, value):
+        self.__duration = value
+
+    @duration.deleter
+    def name(self):
+        self.__duration = None
 
 class TVProgram:
     def __init__(self, tl):
@@ -32,25 +52,25 @@ class TVProgram:
         [self.items.remove(i) for i in self.items if i.uid == indx]
 
 
-assert hasattr(TVProgram, 'add_telecast') and hasattr(TVProgram,
-                                                      'remove_telecast'), "в классе TVProgram должны быть методы add_telecast и remove_telecast"
+assert hasattr(TVProgram, 'add_telecast') and hasattr(TVProgram, 'remove_telecast'), "в классе TVProgram должны быть методы add_telecast и remove_telecast"
 
 pr = TVProgram("Первый канал")
 pr.add_telecast(Telecast(1, "Доброе утро", 10000))
 pr.add_telecast(Telecast(3, "Новости", 2000))
 t = Telecast(2, "Интервью с Балакиревым", 20)
+pr.add_telecast(t)
 
 pr.remove_telecast(3)
 assert len(pr.items) == 2, "неверное число телеперач, возможно, некорректно работает метод remove_telecast"
-assert pr.items[
-           -1] == t, "удалена неверная телепередача (возможно, вы удаляете не по __id, а по порядковому индексу в списке items)"
+assert pr.items[-1] == t, "удалена неверная телепередача (возможно, вы удаляете не по __id, а по порядковому индексу в списке items)"
 
-assert type(Telecast.uid) == property and type(Telecast.name) == property and type(
-    Telecast.duration) == property, "в классе Telecast должны быть объекты-свойства uid, name и duration"
+assert type(Telecast.uid) == property and type(Telecast.name) == property and type(Telecast.duration) == property, "в классе Telecast должны быть объекты-свойства uid, name и duration"
 
 for x in pr.items:
     assert hasattr(x, 'uid') and hasattr(x, 'name') and hasattr(x, 'duration')
 
+[print(i.__dict__) for i in pr.items]
+print(pr.items[0].__dict__)
 assert pr.items[0].name == "Доброе утро", "объект-свойство name вернуло неверное значение"
 assert pr.items[0].duration == 10000, "объект-свойство duration вернуло неверное значение"
 
