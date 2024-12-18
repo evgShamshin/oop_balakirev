a1bf5874b14e5871c046f6ffc5ec8aad56a2abf6
evg_shamshin <i@eshamshin.ru>
2024-12-18 21:58:40 +0300

    2024.12.17

---
+++ 3_methods/3-1___setattr__, __getattribute__, __getattr__ and __delattr__/3.10.09.py
@@ -0,0 +1,50 @@
+class Dimensions:
+    MIN_DIMENSION = 10
+    MAX_DIMENSION = 1000
+
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    """def __setattr__(self, key, value):
+        if key in ('MIN_DIMENSION', 'MAX_DIMENSION'):
+            raise AttributeError("Менять атрибуты MIN_DIMENSION и MAX_DIMENSION запрещено.")
+        if key in ('a', 'b', 'c') and self.MIN_DIMENSION <= value <= self.MAX_DIMENSION:
+            self.__dict__[key] = value
+        if key in ('a', 'b', 'c') and self.MIN_DIMENSION > value > self.MAX_DIMENSION:
+            self.__dict__[key] = __dict__[key]"""
+
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, value):
+        self.__a = value
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, value):
+        self.__b = value
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, value):
+        self.__c = value
+
+
+d = Dimensions(10.5, 20.1, 30)
+print(d.__dict__)
+d.a = 8
+d.b = 15
+print(d.__dict__)
+a, b, c = d.a, d.b, d.c  # a=10.5, b=15, c=30
+# d.MAX_DIMENSION = 10  # исключение AttributeError
