c8813a874f250006fd5678fc051430ab2661325d
evg_shamshin <i@eshamshin.ru>
2024-12-25 23:47:05 +0300

    2024.12.25

--- 3_methods/3-2___call__/3.02.09.py
+++ 3_methods/3-2___call__/3.02.09.py
@@ -2,4 +2,15 @@ class InputDigits:
     def __init__(self, func):
         self.__func = func
 
-   def __call__
+    def __call__(self, args):
+        return (self.__func
+                (list(map(lambda x: int(x), args.split()))))
+
+
+@InputDigits
+def input_dg(num):
+    return num
+
+
+res = input_dg(input())
+print(res)
