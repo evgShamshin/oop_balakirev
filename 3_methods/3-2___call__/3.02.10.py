d954a8ee0f380f43dc6cb8f1ffb5102fa9acc504
evg_shamshin <i@eshamshin.ru>
2024-12-28 23:57:08 +0300

    2024.12.26

--- 3_methods/3-2___call__/3.02.10.py
+++ 3_methods/3-2___call__/3.02.10.py
@@ -2,24 +2,41 @@ class InputValues:
     def __init__(self, render):
         self.render = render
 
+    def _check_value(self, value):
+        if str(value)[0] == '-':
+            if all(str(v).isdigit() for v in value[1:]):
+                return int(value)
+                # all(str(v).isdigit() for v in value):
+
+        if all(str(v).isdigit() for v in value):
+            return int(value)
+
+        return None
+
     def __call__(self, func):
-        # def wrapper(num):
-        pass
+        def wrapper(*args, **kwargs):
+            return [self._check_value(v) for v in func(*args, **kwargs).split()]
+
+        return wrapper
 
 
 class RenderDigit:
     def __call__(self, value):
         if str(value)[0] == '-':
             if all(str(v).isdigit() for v in value[1:]):
-                return True
+                return int(value)
                 # all(str(v).isdigit() for v in value):
 
         if all(str(v).isdigit() for v in value):
-            return True
+            return int(value)
 
-        return False
+        return str(value)
 
 
-render = RenderDigit()
-r1 = render("a12")
-print(r1)
+@InputValues(render=RenderDigit())
+def input_dg():
+    return input()
+
+
+res = input_dg()
+print(res)
