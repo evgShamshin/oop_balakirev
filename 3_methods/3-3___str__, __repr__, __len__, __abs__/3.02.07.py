2c58dbde13d7a20b94c2df79536fb524ec5de193
evg_shamshin <i@eshamshin.ru>
2025-01-01 13:34:32 +0300

    2025.01.01

--- 3_methods/3-3___str__, __repr__, __len__, __abs__/3.02.07.py
+++ 3_methods/3-3___str__, __repr__, __len__, __abs__/3.02.07.py
@@ -2,11 +2,11 @@ from math import sqrt
 
 
 class RadiusVector:
-    def __init__(self, n, *args):
-        if len(args) == 0:
-            self.coords = [0 for _ in range(n)]
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.coords = [0] * args[0]
         else:
-            self.coords = [i for i in args]
+            self.coords = [_ for _ in args]
 
     def set_coords(self, *args):
         for n in range(len(self.coords)):
@@ -24,9 +24,7 @@ class RadiusVector:
 
 
 # TEST-TASK___________________________________
-from math import sqrt
-
-vector3D = RadiusVector(3)
+"""vector3D = RadiusVector(3)
 print(vector3D.__dict__)
 k = str(*[_ for _ in vector3D.__dict__.keys()])
 
@@ -34,7 +32,10 @@ assert len(vector3D.__dict__[k]) == 3, "ошибка в длине списка 
 assert all(True if _ == 0 else False for _ in vector3D.__dict__[k]), "ошибка, значения должны быть нулями"
 
 vector3D.set_coords(3, -5.6, 8)
-"""k = str(*[_ for _ in vector3D.__dict__.keys()])
+print(vector3D.__dict__)
+a, b, c = vector3D.get_coords()
+print(a, b, c)
+k = str(*[_ for _ in vector3D.__dict__.keys()])
 assert len(vector3D.__dict__[k]) == 3 and \
        (vector3D.__dict__[k] == [3, -5.6, 8] or
         vector3D.__dict__[k] == (3, -5.6, 8)), "значения координат неправильные"
@@ -60,3 +61,21 @@ assert len(vector3D) == 3, "неправильно работает метод l
 
 assert abs(vector3D) == sqrt(sum([i * i for i in vector3D.__dict__[k]])), "метод abs() вернул неверное значение"
 print("Правильное решение !")"""
+
+vector3D = RadiusVector(3)
+print(vector3D.__dict__)
+vector3D.set_coords(3, -5.6, 8)
+print(vector3D.__dict__)
+a, b, c = vector3D.get_coords()
+print(a, b, c)
+vector3D.set_coords(4, 3, 999, 10, 11)  # ошибки быть не должно, последние две координаты игнорируются
+print(vector3D.__dict__)
+vector3D.set_coords(1, 2)  # ошибки быть не должно, меняются только первые две координаты
+print(vector3D.__dict__)
+res_len = len(vector3D)  # res_len = 3
+print(res_len)
+res_abs = abs(vector3D)
+print(res_abs)
+
+vector3D = RadiusVector(2, 4)
+print(vector3D.get_coords())
