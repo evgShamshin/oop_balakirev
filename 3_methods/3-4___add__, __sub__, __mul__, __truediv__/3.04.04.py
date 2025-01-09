69bf4a834a43661aaf17350fe8513a05b26063af
evg_shamshin <i@eshamshin.ru>
2025-01-09 23:44:16 +0300

    2025.01.09

--- 3_methods/3-4___add__, __sub__, __mul__, __truediv__/3.04.04.py
+++ 3_methods/3-4___add__, __sub__, __mul__, __truediv__/3.04.04.py
@@ -1,6 +1,6 @@
 class NewList:
-    def __init__(self, value=[]):
-        self.value = value
+    def __init__(self, value=None):
+        self.value = value[:] if value and type(value) == list else []
 
     @staticmethod
     def list_sub_list(a, b):
@@ -8,14 +8,15 @@ class NewList:
         return [x for x in a if (x, type(x)) not in b or b.remove((x, type(x)))]
 
     def __sub__(self, other):
-        slf_val = self if isinstance(self, list) else self.value
-        oth_val = other if isinstance(other, list) else other.value
+        slf_val = self.copy() if isinstance(self, list) else self.value.copy()
+        oth_val = other.copy() if isinstance(other, list) else other.value.copy()
 
         res = [[slf, type(slf)] for slf in slf_val]
         for slf in slf_val:
             for oth in oth_val:
                 if (oth, type(oth)) == (slf, type(slf)):
                     res.remove([slf, type(slf)])
+                    oth_val.remove(oth)
 
         return NewList([r[0] for r in res])
 
@@ -29,21 +30,6 @@ class NewList:
         return self.value
 
 
-lst1 = NewList([1, 2, -4, 6, 10, 11, 15, False, True])
-lst2 = NewList([0, 1, 2, 3, True])
-res_1 = lst1 - lst2  # NewList: [-4, 6, 10, 11, 15, False]
-lst1 -= lst2  # NewList: [-4, 6, 10, 11, 15, False]
-res_2 = lst2 - [0, True]  # NewList: [1, 2, 3]
-print(f"res_1 - {res_1.__dict__}")
-print(f"lst1 - {lst1.__dict__}")
-print(f"res_2 - {res_2.__dict__}")
-
-res_3 = [1, 2, 3, 4.5] - res_2  # NewList: [4.5]
-print(f"res_3 - {res_3.__dict__}")
-a = NewList([2, 3])
-res_4 = [1, 2, 2, 3] - a  # NewList: [1, 2]
-print(f"res_4 - {res_4.__dict__}")
-
 # TEST-TASK___________________________________
 lst = NewList()
 lst1 = NewList([0, 1, -3.4, "abc", True])
@@ -55,7 +41,7 @@ res1 = lst1 - lst2
 res2 = lst1 - [0, True]
 res3 = [1, 2, 3, 4.5] - lst2
 lst1 -= lst2
-
+print(res3.get_list())
 assert res1.get_list() == [-3.4, "abc"], "метод get_list вернул неверный список"
 assert res2.get_list() == [1, -3.4, "abc"], "метод get_list вернул неверный список"
 assert res3.get_list() == [2, 3, 4.5], "метод get_list вернул неверный список"
