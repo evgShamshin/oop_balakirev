74f55431ebb1622c263905694bbcb9de8ce14957
evg_shamshin <i@eshamshin.ru>
2025-01-17 21:04:01 +0300

    2025.01.17

---
+++ 3_methods/3-6___eq__ и __hash__/3.6.4.py
@@ -0,0 +1,24 @@
+class Rect:
+    def __init__(self, x, y, width, height):
+        if type(x) in [int, float]:
+            self.x = x
+        if type(x) in [int, float]:
+            self.y = y
+        if type(width) in [int, float]:
+            self.width = width
+        if type(height) in [int, float]:
+            self.height = height
+
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
+
+    def __hash__(self):
+        return hash((self.width, self.height))
+
+
+r1 = Rect(10, 5, 100, 50)
+r2 = Rect(-10, 4, 100, 50)
+
+h1, h2 = hash(r1), hash(r2)  # h1 == h2
+
+print(h1 == h2)
