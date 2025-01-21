7125099d668681063de7d950dbda565ec1edcfc9
evg_shamshin <i@eshamshin.ru>
2025-01-21 23:58:03 +0300

    2025.01.19

--- 3_methods/3-6___eq__ и __hash__/3.6.7.py
+++ 3_methods/3-6___eq__ и __hash__/3.6.7.py
@@ -5,3 +5,16 @@ class DataBase:
 
     def write(self, record):
         self.dict_db[record] = record
+
+    def read(self, pk):
+        return self.dict_db[pk]
+
+
+class Record:
+    def __init__(self, fio, descr, old):
+        if type(fio) == str:
+            self.fio = fio
+        if type(descr) == str:
+            self.descr = descr
+        if type(old) == int:
+            self.old = old
