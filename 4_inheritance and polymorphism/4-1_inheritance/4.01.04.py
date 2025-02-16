73284b7e3cbb247e5f8387d249ecc49914eefc2f
evg_shamsin <i@eshamshin.ru>
2025-02-16 23:54:22 +0300

    2025.02.15

--- 4_inheritance and polymorphism/4-1_inheritance/4.01.04.py
+++ 4_inheritance and polymorphism/4-1_inheritance/4.01.04.py
@@ -21,18 +21,47 @@ class Cat(Animal):
         valid_data_type = {'old': int,
                            'name': str,
                            'color': str,
-                           'weight': int,
-                           'old_d': value > 0,
-                           'weight_d': value > 0}
+                           'weight': (int, float)}
 
+        if key == "weight":
+            if type(value) in valid_data_type[key]:
+                if value > 0:
+                    self.__dict__[key] = value
 
         if key in valid_data_type.keys():
             if valid_data_type[key] == type(value):
                 self.__dict__[key] = value
 
+    def get_info(self):
+        return f"{self.name}: {self.old}, {self.color}, {self.weight}"
+
+
 class Dog(Animal):
+    def __init__(self, name, old, breed, size):
+        super().__init__(name, old)
+        self.breed = breed
+        self.size = size
+
+    def __setattr__(self, key, value):
+        valid_data_type = {'old': int,
+                           'name': str,
+                           'breed': str,
+                           'size': tuple}
+
+        if key == "size":
+            if valid_data_type[key] == type(value):
+                if all(type(i) == int for i in value):
+                    self.__dict__[key] = value
+
+        if key in valid_data_type.keys():
+            if valid_data_type[key] == type(value):
+                self.__dict__[key] = value
+
+    def get_info(self):
+        return f"{self.name}: {self.old}, {self.breed}, {self.size}"
 
 
-
-c1 = Cat('123', 5, 'green', 1)
-print(c1.__dict__)
+c1 = cat = Cat('cat', 5, 'green', 2)
+# cat: 5, green, 2
+print(c1.get_info() == 'cat: 5, green, 2')
+print(c1.get_info())
